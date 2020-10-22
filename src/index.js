3a1cfd7ab7aeeadcdcf9fb1a461b9f59dd9b35a6
Christian <ChQu00223@gmail.com>
2020-10-22 10:22:45 -0300

    Seciton 2 JSX Versus HTML

--- src/index.js
+++ src/index.js
@@ -2,13 +2,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 // Create a react component
 const App = function () {
-    return <div>Hi there!</div>
-}
+    const buttonText = {text: 'Click me'};
+     
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter Name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {buttonText.text}
+            </button>
+        </div>
+    )
+} 
 
 //Take the react component and show it on the screen
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
 )
+// Differences between jsx and html
+// 1. In line styling - curly braces, camelCase instead of hyphens, strings
+// 2. Use 'className' instead of 'class' - avoids colision of js keyword class
+// 3. JSX can very easily reference JS variables using {}
